e6fd6d19336b1ace6aef7416d495cae7928d98d4
Gregory Langlais <general@gregorylanglais.com>
2015-05-22 15:07:52 -0400

    routing for lander

--- lander.php
+++ lander.php
@@ -17,4 +17,37 @@ License: MIT
 
 if (!defined('ABSPATH')) {die();}
 
+
+function lander_rewrite_tag() {
+    add_rewrite_tag('%lander%', '([^&]+)');
+}
+add_action('init', 'lander_rewrite_tag', 10, 0);
+
+
+function lander_rewrite() {
+    add_rewrite_rule('^lander/(.*)', 'index.php?lander=$matches[1]', 'top');
+    flush_rewrite_rules(false); // todo fixme
+}
+add_action('init', 'lander_rewrite');
+
+
+function lander_template($original_template) {
+    $lander = get_query_var('lander');
+    $template = get_query_template('lander', array($lander . '.php'));
+
+    if (!empty($template)) {
+        return $template;
+    } else {
+        if (!empty($lander)) {
+            global $wp_query;
+            $wp_query->is_404 = true;
+            return get_404_template();
+        } else {
+            return $original_template;
+        }
+    }
+}
+add_filter('template_include', 'lander_template');
+
+
 // todo build out plugin bootstrap and includes
