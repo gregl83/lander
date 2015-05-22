1fd0b3ca0541eb107ade2553ec13c3881e014f66
Gregory Langlais <general@gregorylanglais.com>
2015-05-22 12:05:56 -0400

    wp plugin structure

---
+++ lander.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package wp-lander
+ * @version 0.1.0
+ */
+
+/*
+Plugin Name: WordPress Lander
+Plugin URI: https://github.com/gregl83/wp-lander
+Description: Manage landing pages for WordPress sites
+Version: 0.1.0
+Author: Gregory Langlais
+Author URI: https://github.com/gregl83
+License: MIT
+*/
+
+if (!defined('ABSPATH')) {die();}
+
+// todo build out plugin bootstrap and includes
---
+++ readme.txt
@@ -0,0 +1,7 @@
+=== Plugin Name ===
+Contributors: gregl83
+Tags: lander, landing, page, lead, land, generation, marketing, campaign
+Requires at least: 3.1
+Tested up to: 4.2.2
+Stable tag: 0.1.0
+License: MIT
